95e316bba3ca773c7529f094f5a4f27f796050ef
Wilfrido TA <a01664769@tec.mx>
2025-03-20 22:22:01 -0600

    Numeros centrados

--- memory.py
+++ memory.py
@@ -73,7 +73,10 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+	
+	#Centrar el numero
+        goto(x + 10, y + 5)
+	
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
